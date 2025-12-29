d34d2778947d63dbdee2bc5f360105f94ee2cc20
lxcidh <taylor20050825@gmail.com>
2025-12-29 07:07:28 +0000

    Added support for HTML files

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:
 	mkdir build
 	nasm -f elf64 src/main.asm -o build/main.o
-	ld build/main.o -o build/http-server
+	ld build/main.o -o http-server
 
 clean:
 	rm -rf build
--- src/main.asm
+++ src/main.asm
@@ -4,8 +4,13 @@ section .bss
     socket_fd: resq 1
     accept_fd: resq 1
     buffer: resq 100
-
+    FILENAME_START: resq 1 ; (8 bytes)
+    FILENAME_END: resq 1 ; (8 bytes)
+    file_buffer: resb 4096  ; (4KB)
 section .data
+    delimiter: db ' '
+    index_html_str: db "index.html", 0
+
     http_response:
         ; Status line
         db "HTTP/1.1 200 OK", 0x0D, 0x0A
@@ -16,11 +21,6 @@ section .data
 
         ; The blank line
         db 0x0D, 0x0A
-
-        ; The body
-        db "<h1>Hello from ASM!</h1>", 0x0D, 0x0A
-        db "<p>This is insane, we're literally in ASM rn</p>", 0x0D, 0x0A
-
     http_response_len equ $-http_response
     
     ; The sockaddr_in struct (16 bytes)
@@ -28,7 +28,7 @@ section .data
         dw 2            ; sin_family: AF_INET (2 bytes) -> 0x0002
                         ; (x86 stores this as 02 00, which is fine for local constants)
         
-        db 0x1F, 0x90   ; sin_port: port 8080 (2 bytes)
+        db 0x1F, 0x91   ; sin_port: port 8080 (2 bytes)
                         ; We write the high byte (1F), then the low byte (90)
                         ; so memory is [1F][90] (big endian)
 
@@ -60,13 +60,17 @@ _start:
     mov rdx, 16         ; struct size in bytes
     syscall
 
+    ; Check for bind errors
+    cmp rax, 0
+    jl handle_error
+
     ; SYS_LISTEN - wait for connections
     mov rax, 50
     mov rdi, [socket_fd]
     mov rsi, 10
     syscall
 
-SYS_ACCEPT:    ; Handshake
+accept_loop:    ; Handshake
     mov rax, 43
     mov rdi, [socket_fd]
     mov rsi, 0
@@ -76,7 +80,6 @@ SYS_ACCEPT:    ; Handshake
     cmp rax, 0
     jl handle_error
     mov [accept_fd], rax
-    syscall
 
     ; SYS_READ - recieve the request
     mov rax, 0
@@ -85,24 +88,105 @@ SYS_ACCEPT:    ; Handshake
     mov rdx, 800
     syscall 
 
-    ; SYS_WRITE - send the response
+    ; Process the file request from client
+    ; Init cursor
+    mov rbx, buffer
+
+find_first_space:
+    cmp byte [rbx], ' '
+    je found_first_space
+    inc rbx
+    jmp find_first_space
+
+found_first_space:
+    inc rbx     ; Skip space we finished the loop on
+    mov [FILENAME_START], rbx
+
+find_second_space:
+    cmp byte [rbx], ' '
+    je found_end
+    inc rbx
+    jmp find_second_space
+
+found_end:
+    mov byte [rbx], 0
+    mov [FILENAME_END], rbx
+
+    sub rbx, [FILENAME_START]
+
+    cmp rbx, 1
+    je single_char
+
+read_file:
+    ; SYS_OPEN
+    mov rax, 2
+    mov rdi, [FILENAME_START]
+    mov rsi, 0
+    syscall
+
+    cmp rax, 0
+    jl close_socket
+
+    ; Save file FD
+    mov r15, rax 
+
+    ; SYS_WRITE - Send the HTML header
     mov rax, 1
     mov rdi, [accept_fd]
     mov rsi, http_response
     mov rdx, http_response_len
     syscall
 
+read_file_loop:
+    ; Read from file into file_buffer
+    mov rdi, r15
+    mov rsi, file_buffer
+    mov rdx, 4096
+    mov rax, 0
+    syscall
+
+    ; Check for EOF
+    cmp rax, 0
+    je close_file
+    
+    ; Print RAX amount of bytes from file_buffer
+    mov rdx, rax
+    mov rsi, file_buffer
+    mov rdi, [accept_fd]
+    mov rax, 1
+    syscall
+
+    jmp read_file_loop
+
+close_file:
+    mov rax, 3
+    mov rdi, r15
+    syscall
+
+close_socket:
+    mov rax, 3
+    mov rdi, [accept_fd]
+    syscall
+
+    jmp accept_loop
+
     ; SYS_CLOSE
     mov rax, 3
     mov rdi, [accept_fd]
     syscall
 
-exit:
-    mov rax, 60
-    mov rdi, 0
-    syscall
+    jmp accept_loop
 
 handle_error:
     mov rax, 60
     mov rdi, 1
     syscall
+
+single_char:
+    mov rcx, [FILENAME_START]
+    cmp byte [rcx], '/'
+    jne read_file
+
+    mov rax, index_html_str
+    mov [FILENAME_START], rax
+    jmp read_file
